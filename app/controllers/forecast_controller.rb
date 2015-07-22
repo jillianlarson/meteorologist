3663828768adf23ef13a7dd402d1d9110cad872a
jillianlarson <jillianlarson@Jillians-MacBook.local>
2015-07-22 00:45:25 -0500

    All controllers pulling from APIs

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -15,18 +15,20 @@ class ForecastController < ApplicationController
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+    @coord_url = "https://api.forecast.io/forecast/6ac4aa0b134529ccd14dd9d6dfa82e7b/"+@lat+","+@lng
 
+    require 'json'
+    parsed_data = JSON.parse(open(@coord_url).read)
 
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -16,18 +16,31 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+    @geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address
 
+    require 'json'
+    parsed_data = JSON.parse(open(@geo_url).read)
 
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-    @current_temperature = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
-    @current_summary = "Replace this string with your answer."
+   latstring = @latitude.to_s
+   longstring = @longitude.to_s
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+   @joint_url = "https://api.forecast.io/forecast/6ac4aa0b134529ccd14dd9d6dfa82e7b/"+latstring+","+longstring
+    require 'json'
+    parsed_joint = JSON.parse(open(@joint_url).read)
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @current_temperature = parsed_joint["currently"]["temperature"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @current_summary = parsed_joint["currently"]["summary"]
+
+    @summary_of_next_sixty_minutes = parsed_joint["minutely"]["summary"]
+
+    @summary_of_next_several_hours = parsed_joint["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_joint["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
